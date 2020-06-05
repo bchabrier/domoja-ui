18a9d9abe41a17cca66db1c92d274d2c6a41e477
Bruno Chabrier <bruno_chabrier@hotmail.com>
2020-06-05 20:39:54 +0200

    Formatting

--- src/providers/websocket/websocket.ts
+++ src/providers/websocket/websocket.ts
@@ -21,7 +21,7 @@ export class WebsocketService {
 
   private create(url): Rx.Subject<MessageEvent> {
 
-     this.socket = io(url);
+    this.socket = io(url);
 
     let observable = Rx.Observable.create((obs: Rx.Observer<MessageEvent>) => {
       this.socket.on('change', obs.next.bind(obs));
